149f141ae10db2cc250f803739453c05c038e874
Shirzart Enwer <shirzartenwer@gmail.com>
2025-09-16 09:50:36 +0200

    feat: backened and its test

---
+++ src/Core/Tests/test_intersector.py
@@ -0,0 +1,56 @@
+from Core.two_collection_intersector import TwoCollectionIntersector
+
+import pytest
+
+
+@pytest.mark.parametrize("size_a, size_b", [(100, 200), (0, 0), (1, 1), (50, 50)])
+def test_initialization(size_a, size_b):
+    intersector = TwoCollectionIntersector.from_sizes(size_a, size_b)
+    assert len(intersector.collection_a) == size_a
+    assert len(intersector.collection_b) == size_b
+
+
+def test_output_number_type(size_a=100, size_b=200):
+    intersector = TwoCollectionIntersector.from_sizes(size_a, size_b)
+    result, run_time = intersector.intersect("Collection A")
+    assert isinstance(result, list)
+    assert all(isinstance(x, float) for x in result)
+    assert isinstance(run_time, float)
+
+    
+@pytest.mark.parametrize("size_a, size_b, collection, expected_length", 
+                          [(0, 100, "Collection A", 0), 
+                           (100, 0, "Collection B", 0), 
+                           (0, 0, "Collection A", 0),
+                           (0, 0, "Collection B", 0)])
+def test_intersect_empty_collections(size_a, size_b, expected_length, collection):
+    intersector = TwoCollectionIntersector.from_sizes(size_a, size_b)
+    result, run_time = intersector.intersect(collection)
+    assert len(result) == expected_length
+    assert run_time >= 0.0
+
+
+
+@pytest.mark.parametrize("collection_a, collection_b, choice, expected_length", 
+                          [([1.0, 2.0, 3.0], [4.0, 5.0, 6.0], "Collection A", 0),
+                           ([1.0, 2.0, 3.0], [4.0, 5.0, 6.0], "Collection B", 0)])
+        
+def test_intersect_no_common_elements(collection_a, collection_b, choice, expected_length):
+    intersector = TwoCollectionIntersector(collection_a, collection_b)
+    result, run_time = intersector.intersect(choice)
+    assert len(result) == expected_length
+    assert run_time >= 0.0
+
+
+
+@pytest.mark.parametrize("collection_a, collection_b, choice, expected_length", 
+                          [([1.0, 2.0, 3.0], [3.0, 5.0, 6.0], "Collection A", 1),
+                           ([1.0, 2.0, 3.0], [3.0, 5.0, 6.0], "Collection B", 1),
+                           ([1.0, 2.0, 4.0], [1.0, 2.0, 3.0], "Collection A", 2),
+                           ([1.0, 2.0, 4.0], [1.0, 2.0, 3.0], "Collection B", 2)])
+        
+def test_intersect_some_common_elements(collection_a, collection_b, choice, expected_length):
+    intersector = TwoCollectionIntersector(collection_a, collection_b)
+    result, run_time = intersector.intersect(choice)
+    assert len(result) == expected_length
+    assert run_time >= 0.0
---
+++ src/Core/two_collection_intersector.py
@@ -0,0 +1,42 @@
+from typing import List, Union
+# from pydantic import BaseModel
+import time
+import random
+
+
+class TwoCollectionIntersector:
+    collection_a: List[float]
+    collection_b: List[float]
+    runtime: float
+
+
+    def __init__(self, collection_a: List[float], collection_b: List[float]):
+        self.collection_a = collection_a
+        self.collection_b = collection_b
+
+    @classmethod
+    def from_sizes(cls, size_a: int, size_b: int) -> "TwoCollectionIntersector":
+        collection_a = [random.random() for _ in range(size_a)]
+        collection_b = [random.random() for _ in range(size_b)]
+        return cls(collection_a, collection_b)
+        
+
+    def intersect(self, choice: str) -> Union[List[float], float]:
+        result = []
+        start_time = time.time()
+        if choice == "Collection A":
+            hash_set = set(self.collection_a)
+            iterator = self.collection_b
+        else:
+            hash_set = set(self.collection_b)
+            iterator = self.collection_a
+
+        
+        for item in iterator:
+            if item in hash_set:
+                result.append(item)
+
+        self.runtime = time.time() - start_time
+        return result, self.runtime
+
+
