7d22965e3bb63b6f6aa56607ca0ff9e37ebe9951
Shirzart Enwer <shirzartenwer@gmail.com>
2025-09-17 11:39:29 +0200

    doc: add config file for parameter numbers

---
+++ src/config.py
@@ -0,0 +1,10 @@
+"""Configuration constants for the intersection application."""
+
+# Collection size constraints
+MIN_COLLECTION_SIZE = 0
+MAX_COLLECTION_SIZE = 1_000_000
+
+# Constants for collection generation
+RANDOM_ELEMENT_MIN = 1
+RANDOM_ELEMENT_MAX = 1000
+VALUE_RANGE_MULTIPLIER = 2
